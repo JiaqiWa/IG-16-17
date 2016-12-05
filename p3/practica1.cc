4d9c6319274687f0a17046184ecd77569c3bc8df
JiaqiWa <susanajiaqi@correo.ugr.es>
2016-12-05 07:58:20 +0100

    Finished P3

---
+++ p3/practica1.cc
@@ -0,0 +1,372 @@
+//**************************************************************************
+// Práctica 1 usando objetos
+//**************************************************************************
+
+#include <GL/glut.h>
+#include <ctype.h>
+#include <math.h>
+#include <vector>
+#include "objetos.h"
+
+
+using namespace std;
+
+
+// variables que definen la posicion de la camara en coordenadas polares
+GLfloat Observer_distance;
+GLfloat Observer_angle_x;
+GLfloat Observer_angle_y;
+
+// variables que controlan la ventana y la transformacion de perspectiva
+GLfloat Size_x,Size_y,Front_plane,Back_plane;
+
+// variables que determninan la posicion y tamaño de la ventana X
+int Window_x=50,Window_y=50,Window_width=450,Window_high=450;
+
+//***************************************************************************
+// objetos
+//***************************************************************************
+_cubo cuboBase(2.0);
+
+float alfa=0.0;
+float beta=0.0;
+float g1=0.0;
+float g2=0.0;
+float g3=0.0;
+float angulo=0.0;
+
+string modo, accion;
+//**************************************************************************
+//
+//***************************************************************************
+
+void clean_window()
+{
+
+glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+}
+
+
+//**************************************************************************
+// Funcion para definir la transformación de proyeccion
+//***************************************************************************
+
+void change_projection()
+{
+
+glMatrixMode(GL_PROJECTION);
+glLoadIdentity();
+
+// formato(x_minimo,x_maximo, y_minimo, y_maximo,plano_delantero, plano_traser)
+//  plano_delantero>0  plano_trasero>PlanoDelantero)
+glFrustum(-Size_x,Size_x,-Size_y,Size_y,Front_plane,Back_plane);
+}
+
+//**************************************************************************
+// Funcion para definir la transformación de vista (posicionar la camara)
+//***************************************************************************
+
+void change_observer()
+{
+
+// posicion del observador
+glMatrixMode(GL_MODELVIEW);
+glLoadIdentity();
+glTranslatef(0,0,-Observer_distance);
+glRotatef(Observer_angle_x,1,0,0);
+glRotatef(Observer_angle_y,0,1,0);
+}
+
+//**************************************************************************
+// Funcion que dibuja los ejes utilizando la primitiva grafica de lineas
+//***************************************************************************
+
+void draw_axis()
+{
+
+glDisable(GL_LIGHTING);
+glLineWidth(2);
+glBegin(GL_LINES);
+// eje X, color rojo
+glColor3f(1,0,0);
+glVertex3f(-AXIS_SIZE,0,0);
+glVertex3f(AXIS_SIZE,0,0);
+// eje Y, color verde
+glColor3f(0,1,0);
+glVertex3f(0,-AXIS_SIZE,0);
+glVertex3f(0,AXIS_SIZE,0);
+// eje Z, color azul
+glColor3f(0,0,1);
+glVertex3f(0,0,-AXIS_SIZE);
+glVertex3f(0,0,AXIS_SIZE);
+glEnd();
+}
+
+
+//**************************************************************************
+// Funcion que dibuja los objetos
+
+
+
+void draw_objects()
+{
+	_objeto objetoPly("beethoven.ply");
+	_objeto cilindroBase("cilindro.ply");
+	if(accion == "1"){
+
+		if (modo == "P")
+		 	objetoPly.draw_puntos(1,0,1,5);
+		else if (modo == "L")
+			objetoPly.draw_aristas(0,1,1,4);
+		else if (modo == "S")
+		 	objetoPly.draw_solido(2,0,1);
+		else if (modo == "A")
+		 	objetoPly.draw_solido_ajedrez(1,1,0,0,0,0.3);
+	}
+	if(accion == "2"){
+
+		_revolucion cilindroRev(cilindroBase,Y, 100.0);
+		if (modo == "P")
+		 	cilindroRev.draw_puntos(1,0,1,5);
+		else if (modo == "L")
+			cilindroRev.draw_aristas(0,1,1,4);
+		else if (modo == "S")
+		 	cilindroRev.draw_solido(2,0,1);
+		else if (modo == "A")
+		 	cilindroRev.draw_solido_ajedrez(1,1,0,0,0,0.3);
+
+	}
+	if(accion == "3")
+	{
+		_objeto cilindroBase("cilindro.ply");
+		_revolucion cilindroRev(cilindroBase,Y, 100.0);
+		_grua grua(cuboBase,cilindroRev,alfa, beta,angulo, g1,g2,g3);
+		grua.dibuja(modo);
+	}
+
+}
+
+
+//**************************************************************************
+//
+//***************************************************************************
+
+void draw(void)
+{
+clean_window();
+change_observer();
+draw_axis();
+draw_objects();
+glutSwapBuffers();
+}
+
+
+
+//***************************************************************************
+// Funcion llamada cuando se produce un cambio en el tamaño de la ventana
+//
+// el evento manda a la funcion:
+// nuevo ancho
+// nuevo alto
+//***************************************************************************
+
+void change_window_size(int Ancho1,int Alto1)
+{
+float Aspect_ratio;
+
+Aspect_ratio=(float) Alto1/(float )Ancho1;
+Size_y=Size_x*Aspect_ratio;
+change_projection();
+glViewport(0,0,Ancho1,Alto1);
+glutPostRedisplay();
+}
+
+
+//***************************************************************************
+// Funcion llamada cuando se aprieta una tecla normal
+//
+// el evento manda a la funcion:
+// codigo de la tecla
+// posicion x del raton
+// posicion y del raton
+//***************************************************************************
+
+void normal_key(unsigned char Tecla1,int x,int y)
+{
+	if (toupper(Tecla1) == 'Q')
+		exit(0);
+	else if (toupper(Tecla1) == '8' )
+	{
+		alfa=alfa+2.0;
+		angulo = 0.0;
+	}
+	else if (toupper(Tecla1)=='5')
+	{
+		alfa=alfa-2.0;
+		angulo = 0.0;
+	}
+	else if (toupper(Tecla1)=='6'){
+		beta=beta-2.0;
+		angulo = angulo - 2;
+		if(angulo < -50.0)
+			angulo=-50.0;
+	}
+	else if (toupper(Tecla1)=='4'){
+		beta=beta+2.0;
+		angulo = angulo+ 2;
+		if(angulo > 50.0)
+			angulo=50.0;
+	}
+	else if (Tecla1 == 'z' )
+		g1=g1+1.0;
+	else if (Tecla1=='Z')
+		g1=g1-1.0;
+	else if (Tecla1 == 'x' )
+	{
+		g2=min(80.0,g2+1.0);
+		//if(g2 > 3.0)
+		//	g2=3.0;
+	}
+	else if (Tecla1=='X')
+	{
+		g2=max(0.0,g2-1.0);
+		//if(g2 < -5.0)
+		//	g2=-5.0;
+	}
+	else if (Tecla1 == 'c' )
+	{
+		g3=g3+1.0;
+		if(g3 > 12.0)
+			g3=12.0;
+	}
+	else if (Tecla1=='C')
+	{
+		g3=g3-1.0;
+		if(g3 < 0.0)
+			g3=0.0;
+	}
+	else if(Tecla1=='p')
+		modo="P";
+	else if(Tecla1=='l')
+		modo="L";
+	else if(Tecla1=='s')
+		modo="S";
+	else if(Tecla1=='a')
+		modo="A";
+	else if(Tecla1=='1')
+		accion="1";
+	else if(Tecla1=='2')
+		accion="2";
+	else if(Tecla1=='3')
+		accion="3";
+
+	glutPostRedisplay();
+
+
+}
+
+//***************************************************************************
+// Funcion llamada cuando se aprieta una tecla especial
+//
+// el evento manda a la funcion:
+// codigo de la tecla
+// posicion x del raton
+// posicion y del raton
+
+//***************************************************************************
+
+void special_key(int Tecla1,int x,int y)
+{
+
+switch (Tecla1){
+	case GLUT_KEY_LEFT:Observer_angle_y--;break;
+	case GLUT_KEY_RIGHT:Observer_angle_y++;break;
+	case GLUT_KEY_UP:Observer_angle_x--;break;
+	case GLUT_KEY_DOWN:Observer_angle_x++;break;
+	case GLUT_KEY_PAGE_UP:Observer_distance*=1.2;break;
+	case GLUT_KEY_PAGE_DOWN:Observer_distance/=1.2;break;
+	}
+glutPostRedisplay();
+}
+
+
+
+//***************************************************************************
+// Funcion de incializacion
+//***************************************************************************
+
+void initialize(void)
+{
+// se inicalizan la ventana y los planos de corte
+Size_x=5;
+Size_y=5;
+Front_plane=10;
+Back_plane=1000;
+
+// se incia la posicion del observador, en el eje z
+Observer_distance=2*Front_plane;
+Observer_angle_x=0;
+Observer_angle_y=0;
+
+// se indica cual sera el color para limpiar la ventana	(r,v,a,al)
+// blanco=(1,1,1,1) rojo=(1,0,0,1), ...
+glClearColor(1,1,1,1);
+
+// se habilita el z-bufer
+glEnable(GL_DEPTH_TEST);
+change_projection();
+glViewport(0,0,Window_width,Window_high);
+
+}
+
+
+//***************************************************************************
+// Programa principal
+//
+// Se encarga de iniciar la ventana, asignar las funciones e comenzar el
+// bucle de eventos
+//***************************************************************************
+
+int main(int argc, char **argv)
+{
+
+// se llama a la inicialización de glut
+glutInit(&argc, argv);
+
+// se indica las caracteristicas que se desean para la visualización con OpenGL
+// Las posibilidades son:
+// GLUT_SIMPLE -> memoria de imagen simple
+// GLUT_DOUBLE -> memoria de imagen doble
+// GLUT_INDEX -> memoria de imagen con color indizado
+// GLUT_RGB -> memoria de imagen con componentes rojo, verde y azul para cada pixel
+// GLUT_RGBA -> memoria de imagen con componentes rojo, verde, azul y alfa para cada pixel
+// GLUT_DEPTH -> memoria de profundidad o z-bufer
+// GLUT_STENCIL -> memoria de estarcido
+glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+
+// posicion de la esquina inferior izquierdad de la ventana
+glutInitWindowPosition(Window_x,Window_y);
+
+// tamaño de la ventana (ancho y alto)
+glutInitWindowSize(Window_width,Window_high);
+
+// llamada para crear la ventana, indicando el titulo (no se visualiza hasta que se llama
+// al bucle de eventos)
+glutCreateWindow("PRACTICA 2");
+
+// asignación de la funcion llamada "dibujar" al evento de dibujo
+glutDisplayFunc(draw);
+// asignación de la funcion llamada "change_window_size" al evento correspondiente
+glutReshapeFunc(change_window_size);
+// asignación de la funcion llamada "normal_key" al evento correspondiente
+glutKeyboardFunc(normal_key);
+// asignación de la funcion llamada "tecla_Especial" al evento correspondiente
+glutSpecialFunc(special_key);
+
+// funcion de inicialización
+initialize();
+
+// inicio del bucle de eventos
+glutMainLoop();
+return 0;
+}
